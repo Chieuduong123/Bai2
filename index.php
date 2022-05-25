08ccf0bce57082df3b2f164dab749de22e580686
Chieuduong123 <ntaduong.20it2@vku.udn.vn>
2022-05-25 13:46:38 +0700

    make it better

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>BÀI TẬP 2</h1>
+    <a href="bai1.php">Bài 1</a>
+    <a href="bai2.php">Bài 2</a>
+    <a href="bai3.php">Bài 3</a>
+    <a href="bai4.php">Bài 4</a>
+    <a href="bai5.php">Bài 5</a>
+    <a href="bai6.php">Bài 6</a>
+    <a href="bai7.php">Bài 7</a>
+    <a href="bai8.php">Bài 8</a>
+    <a href="bai9.php">Bài 9</a>
+    <a href="bai10.php">Bài 10</a>
+</body>
+</html>
