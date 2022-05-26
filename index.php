5b4b1582f9227fb4d287ea10799b6f0080374949
Chieuduong123 <ntaduong.20it2@vku.udn.vn>
2022-05-26 11:45:25 +0700

    updatecode

--- index.php
+++ index.php
@@ -13,15 +13,15 @@
 <body>
     <h1 style="text-align:center ;">Bài 2</h1><br><br><br>
     <h2  style="text-align:center ;"> <a href="bai1.php">Bài 1</a>
-    <a href="bai2.php">Bài 2</a>
-    <a href="bai3.php">Bài 3</a>
-    <a href="bai4.php">Bài 4</a>
-    <a href="bai5.php">Bài 5</a>
-    <a href="bai6.php">Bài 6</a>
-    <a href="bai7.php">Bài 7</a>
-    <a href="bai8.php">Bài 8</a>
-    <a href="bai9.php">Bài 9</a>
-    <a href="bai10.php">Bài 10</a></h2><br><br>
+    <a href="bai2/bai2.php">Bài 2</a>
+    <a href="bai2/bai3.php">Bài 3</a>
+    <a href="bai2/bai4.php">Bài 4</a>
+    <a href="bai2/bai5.php">Bài 5</a>
+    <a href="bai2/bai6.php">Bài 6</a>
+    <a href="bai2/bai7.php">Bài 7</a>
+    <a href="bai2/bai8.php">Bài 8</a>
+    <a href="bai2/bai9.php">Bài 9</a>
+    <a href="bai2/bai10.php">Bài 10</a></h2><br><br>
     <h1 style="text-align:center ;">Bài 3</h1><br><br><br>
     <h2 style="text-align:center ;"> <a href="bai3/login.php">Login</a> </h2><br><br>
     <h2 style="text-align:center ;"> <a href="bai3/register.php">Register</a> </h2>
