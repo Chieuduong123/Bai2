c2ec5cc554f2be915b84aad7c0fda14ce03126af
Chieuduong123 <ntaduong.20it2@vku.udn.vn>
2022-05-26 13:34:06 +0700

    updatecode

--- bai3/login_a.php
+++ bai3/login_a.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 header('Content-Type: text/html; charset=UTF-8');
-if (isset($_POST['login'])) 
-{
+if (isset($_POST['login'])) {
     include('database.php');
     $username = addslashes($_POST['username']);
     $password = $_POST['password'];
@@ -10,19 +9,16 @@ if (isset($_POST['login']))
         echo "Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu. <a href='javascript: history.go(-1)'>Trở lại</a>";
         exit;
     }
-     
+
     $query = "SELECT username, password FROM members WHERE username = '$username' and password = '$password'";
 
     $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-     
+
     $row = mysqli_fetch_array($result);
-     
+
     if ($password != $row['password']) {
         echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
         exit;
     }
-    $_SESSION['username'] = $username;
-    echo "Xin chào " . $username . ". Bạn đã đăng nhập thành công</a>";
-    die();
+    include('home.php');
 }
-?>
